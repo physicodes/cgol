68a7df75c0c6abb783b7be1c657461c8581c73bf
Joshua Read <josh@beans.local>
2019-12-10 09:54:44 +0000

    Add constants.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+const ALIVE: bool = true;
+const DEAD: bool = false;
+
+const WIDTH: i32 = 100;
+const HEIGHT: i32 = 40;
+const SIZE: i32 = WIDTH * HEIGHT;
+
 fn main() {
     println!("Hello, world!");
 }
