d72f4a14864636a19c39cc48f44705f1d16918ac
Joshua Read <josh@beans.lan>
2019-12-21 21:48:57 +0000

    Reimplement CGOL visualisation with enhanced structure

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use rand::Rng;
-// use std::{thread, time};
-// 
-// const FRAC_ALIVE: f64 = 1./4.;
-// 
-// const WIDTH: usize = 60;
-// const HEIGHT: usize = 20;
-// const SIZE: usize = WIDTH * HEIGHT;
+use std::{thread, time::Duration};
 
 #[derive(Debug)]
 enum State {
@@ -14,36 +8,28 @@ enum State {
 }
 
 #[derive(Debug)]
-struct Cell {
-    state: State,
-}
-
-#[derive(Debug)]
 struct Board {
     width: i32,
     height: i32,
-    size: i32,
-    cells: Vec<Cell>,
+    cells: Vec<State>,
 }
 
 impl Board {
 
     fn init(width: i32, height: i32, frac_alive: f64) -> Board {
 
-        let size = width * height;
-
         let mut cells = Vec::new();
         let mut rng = rand::thread_rng();
-        for _ in 0..size {
+        for _ in 0..(width * height) {
             let p: f64 = rng.gen();
             if p < frac_alive {
-                cells.push(Cell {state: State::Alive})
+                cells.push(State::Alive)
             } else {
-                cells.push(Cell {state: State::Dead})
+                cells.push(State::Dead)
             }
         }
 
-        Board {width: width, height: height, size: size, cells: cells}
+        Board {width: width, height: height, cells: cells}
     }
 
     fn print_board(&self) {
@@ -60,7 +46,7 @@ impl Board {
             }
 
             // Hashtag for alive, space for dead
-            match cell.state {
+            match cell {
                 State::Alive => output.push('#'),
                 State::Dead  => output.push(' '),
             }
@@ -70,172 +56,139 @@ impl Board {
         println!("{}", output);
     }
 
-    fn count_neighbours(&self, index: i32) {
+    fn pos_from_ind(&self, index: i32) -> (i32, i32) {
+        let x = index % self.width;
+        let y = index / self.width;
+        (x, y)
+    }
 
-        // get neighbour indices
-        let nw = self.validate_index(index - self.width - 1);
-        let nn = self.validate_index(index - self.width);
-        let ne = self.validate_index(index - self.width + 1);
-        let ww = self.validate_index(index - 1);
-        let ee = self.validate_index(index + 1);
-        let sw = self.validate_index(index + self.width - 1);
-        let ss = self.validate_index(index + self.width);
-        let se = self.validate_index(index + self.width + 1);
+    fn ind_from_pos(&self, position: (i32, i32)) -> usize {
+        (self.width * position.1 + position.0) as usize
+    }
 
-        // count living neighbours
-        let mut sum = 0i32;
-        for i in [nw, nn, ne, ww, ee, sw, ss, se].iter() {
-            match self.cells[*i].state {
+    fn verify_pos(&self, position: (i32, i32)) -> (i32, i32) {
+
+        let mut x = position.0;
+        if x == -1 {
+            x = self.width - 1;
+        } else if x == self.width {
+            x = 0;
+        }
+
+        let mut y = position.1;
+        if y == -1 {
+            y = self.height - 1;
+        } else if y == self.height {
+            y = 0;
+        }
+
+        (x, y)
+    }
+
+    fn get_neighbours(&self, index: usize) -> [usize; 8] {
+        // defo wanna tidy this up with a macro
+        let (x, y) = self.pos_from_ind(index as i32);
+        [
+            self.ind_from_pos(self.verify_pos((x-1, y-1))),
+            self.ind_from_pos(self.verify_pos((x, y-1))),
+            self.ind_from_pos(self.verify_pos((x+1, y-1))),
+            self.ind_from_pos(self.verify_pos((x-1, y))),
+            self.ind_from_pos(self.verify_pos((x+1, y))),
+            self.ind_from_pos(self.verify_pos((x-1, y+1))),
+            self.ind_from_pos(self.verify_pos((x, y+1))),
+            self.ind_from_pos(self.verify_pos((x+1, y+1))),
+        ]
+
+    }
+
+    fn count_neighbours(&self, index: usize) -> i32 {
+        let neighbour_indices = self.get_neighbours(index);
+        let mut sum: i32 = 0;
+        for i in neighbour_indices.iter() {
+            match self.cells[*i] {
                 State::Alive => sum += 1,
-                State::Dead  => (),
+                State::Dead => (),
             }
         }
-        let n = 5i32;
-        println!("{}", n);
+        sum
     }
 
-    fn validate_index(&self, mut index: i32) -> usize {
-        // Check for wrapping at top and bottom
-        println!("{}", index);
-        if (index + 1) < 0 {
-            index += self.size;
-        } else if (index - 1) > self.size {
-            index -= self.size;
+    fn update(&mut self) {
+        let mut new_cells: Vec<State> = Vec::new();
+
+        // let updated_cells: Vec<State> = Vec::new();
+        for (i, state) in self.cells.iter().enumerate() {
+            let num_neighbours = self.count_neighbours(i);
+            let new_state: State;
+
+            if num_neighbours == 3 {
+                new_state = State::Alive
+            } else if num_neighbours == 2 {
+                new_state = match state {
+                    State::Alive => State::Alive,
+                    State::Dead => State::Dead,
+                };
+            } else {
+                new_state = State::Dead;
+            }
+            new_cells.push(new_state);
         }
 
-        // Check for wrapping at edges
-        if ((index + 1) % self.width) == 0 {
-            index += self.width;
-        } else if ((index - 1) % self.width) == 0 {
-            index -= self.width;
-        }
-
-        println!("{}", index);
-        return index as usize;
+        self.cells = new_cells;
     }
 
+    fn sum(&self) -> i32 {
+        let mut sum = 0i32;
+        for state in self.cells.iter() {
+            match state {
+                State::Alive => sum += 1,
+                State::Dead => (),
+            };
+        }
+        sum
+    }
+
+}
+
+fn visualise_sim(frac_alive: f64) {
+    let mut b = Board::init(50, 20, frac_alive);
+    loop {
+        print!("\x1B[2J");
+        b.print_board();
+        println!("{} cells alive.", b.sum());
+        b.update();
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+#[test]
+fn test_board() {
+    let mut b = Board::init(5, 5, 1.);
+    let b1 = Board::init(5, 5, 0.);
+
+    b.print_board();
+
+    let ind: usize = 13;
+    let pos = b.pos_from_ind(ind as i32);
+    assert_eq!(pos, (3, 2));
+    assert_eq!(b.ind_from_pos(pos), ind);
+    assert_eq!(b.verify_pos((-1, -1)), (4, 4));
+    assert_eq!(b.verify_pos((2, 3)), (2, 3));
+    assert_eq!(b.verify_pos((5, 5)), (0, 0));
+    assert_eq!(b.get_neighbours(4), [23, 24, 20, 3, 0, 8, 9, 5]);
+    assert_eq!(b.count_neighbours(4), 8);
+    assert_eq!(b1.count_neighbours(4), 0);
+    assert_eq!(b.sum(), 25);
+    b.update();
+    assert_eq!(b.sum(), 0);
 }
 
 fn main() {
-    let b = Board::init(3, 3, 1.);
+    let mut b = Board::init(100, 100, 0.5);
     b.print_board();
-    b.count_neighbours(0i32);
+    println!("{}", b.sum());
+    b.update();
+    b.print_board();
+    println!("{}", b.sum());
+    visualise_sim(0.5);
 }
-
-// 
-//     fn next_state(&mut self) {
-//         if self.neighbours == 3 {
-//             self.state = State::Alive;
-//         } else if self.neighbours == 2 && self.state == State::Alive {
-//             self.state = State::Alive;
-//         } else {
-//             self.state = State::Dead;
-//         }
-//     }
-// }
-// 
-// #[derive(Debug)]
-// struct Board {
-//     width: i32,
-//     height: i32,
-//     size: usize,
-//     frac_alive: f64,
-//     cells: [Cell; SIZE],
-// }
-// 
-// impl Board {
-//     fn init(width: i32, height: i32, frac_alive: f64) -> Board {
-//         size = (width * height) as usize;
-//         cells = [Cell {State::Dead, 0i8}; size];
-//         Board {width, height, size, frac_alive, cells}
-//     }
-// }
-// 
-// fn init_board() -> [bool; SIZE] {
-// 
-//     let mut board: [bool; SIZE] = [false; SIZE];
-//     let mut rng = rand::thread_rng();
-// 
-//     for cell in board.iter_mut() {
-//         let random: f64 = rng.gen();
-//         if random < FRAC_ALIVE {
-//             *cell = true;
-//         }
-//     }
-// 
-//     return board;
-// }
-// 
-// fn modulo(a: i32, b: i32) -> usize {
-//     (((a % b) + b) % b) as usize
-// }
-// 
-// fn get_neighbour_inds(index: i32) -> [usize; 8] {
-//     // this function could probably use a macro to tidy it up
-//     let w = WIDTH as i32;
-//     let s = SIZE as i32;
-//     let nn = modulo(index - w, s);
-//     let nw = modulo(index - w - 1, s);
-//     let ne = modulo(index - w + 1, s);
-//     let ww = modulo(index - 1, s);
-//     let ee = modulo(index + 1, s);
-//     let ss = modulo(index + w, s);
-//     let sw = modulo(index + w - 1, s);
-//     let se = modulo(index + w + 1, s);
-//     return [nw, nn, ne, ww, ee, sw, ss, se];
-// }
-// 
-// fn update_neighbours(board: &[bool; SIZE], neighbours: &mut [u8; SIZE]) {
-//     for i in 0..SIZE {
-//         let mut sum: u8 = 0;
-//         let neighbour_inds = get_neighbour_inds(i as i32);
-//         for j in neighbour_inds.iter() {
-//             if board[*j] == ALIVE {
-//                 sum += 1;
-//             }
-//         }
-//         neighbours[i] = sum;
-//     }
-// }
-// 
-// fn update_board(board: &mut [bool; SIZE], neighbours: &[u8; SIZE]) {
-//     for (state, nr_neighbours) in board.iter_mut().zip(neighbours.iter()) {
-//         if *nr_neighbours == 3 {
-//             *state = ALIVE;
-//         } else if *nr_neighbours == 2 && *state == ALIVE {
-//             *state = ALIVE;
-//         } else {
-//             *state = DEAD;
-//         }
-//     }
-// }
-// 
-// fn main() {
-// 
-//     // Init board and neighbours
-//     let mut board = init_board();
-//     let mut neighbours = [0u8; SIZE];
-// 
-//     // Init loop variables
-//     let mut n: u32 = 0;
-//     let wait_duration = time::Duration::new(REFRESH_RATE, 0);
-// 
-//     // Start loop
-//     loop {
-// 
-//         // Increment counter
-//         n += 1;
-// 
-//         // Display
-//         print!("{}[2J", 27 as char); // clear screen
-//         print_board(&board);
-//         println!("{}th iteration", n);
-// 
-//         // Update
-//         update_neighbours(&board, &mut neighbours);
-//         update_board(&mut board, &neighbours);
-// 
-//         // Wait
-//         thread::sleep(wait_duration);
-//     }
-// }
