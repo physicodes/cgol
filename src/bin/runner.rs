c3c45451418380b478520d3bb8caa60cbae8e658
Joshua Read <josh@beans.lan>
2019-12-23 22:50:20 +0000

    Move simulation script to seperate binary

---
+++ src/bin/runner.rs
@@ -0,0 +1,29 @@
+use std::error::Error;
+use csv::Writer;
+use cgol::Board;
+
+fn run_game(frac_alive: f64, iterations: i32) -> Vec<String> {
+    let mut b = Board::init(100, 100, frac_alive);
+    let mut population = Vec::new();
+
+    for _ in 0..(iterations + 1) {
+        population.push(b.sum().to_string());
+        b.update();
+    }
+
+    population
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut wtr = Writer::from_path("out/sim_results.dat")?;
+    const REPEATS: i32 = 100;
+    let fracs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+    for frac in fracs.iter() {
+        for _ in 0..REPEATS {
+            let results = run_sim(*frac, 100);
+            wtr.write_record(results)?;
+        }
+    }
+    wtr.flush()?;
+    Ok(())
+}
