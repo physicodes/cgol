b40386d543908f23a68806eeb42d8d236d00a277
Joshua Read <josh@beans.lan>
2020-01-19 10:25:06 +0000

    Reimplement csv output from runner.

--- src/bin/runner.rs
+++ src/bin/runner.rs
@@ -1,6 +1,7 @@
 use cgol::Board;
 use std::sync::mpsc;
 use std::thread;
+use csv::Writer;
 
 // Board constants
 const WIDTH: i32 = 100;
@@ -15,23 +16,30 @@ const FULL_RANGE: [f64; 9] = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
 const STRANGE_RANGE: [f64; 6] = [0.10, 0.12, 0.14, 0.16, 0.18, 0.20];
 
 fn threaded_runner(frac: f64) {
-        let (tx, rx) = mpsc::channel();
-        for _ in 0..REPEATS {
-            let tx_clone = mpsc::Sender::clone(&tx);
-            let frac_clone = frac;
-            thread::spawn(move || {
-                let mut board = Board::from_probability(WIDTH, HEIGHT, frac_clone);
-                let results = board.run(ITERATIONS);
-                tx_clone.send(results).unwrap();
-            });
+    let mut wtr = Writer::from_path(format!("analysis/data/{}.csv", frac))
+        .unwrap();
+
+    let (tx, rx) = mpsc::channel();
+    for _ in 0..REPEATS {
+        let tx_clone = mpsc::Sender::clone(&tx);
+        let frac_clone = frac;
+        thread::spawn(move || {
+            let mut board = Board::from_probability(WIDTH, HEIGHT, frac_clone);
+            let results = board.run(ITERATIONS);
+            tx_clone.send(results).unwrap();
+        });
+    }
+
+    let mut n_recieved = 0;
+    for result in rx {
+        n_recieved += 1;
+        wtr.write_record(&result).unwrap();
+        if n_recieved == REPEATS {
+            break;
         }
-        let mut n_recieved = 0;
-        for result in rx {
-            n_recieved += 1;
-            if n_recieved == REPEATS {
-                break;
-            }
-        }
+    }
+
+    wtr.flush().unwrap();
 }
 
 fn main() {
