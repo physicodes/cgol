37f56e28a4ffd34ac2e81e6bd8f7e4c24096098f
Joshua Read <joshua-read@hotmail.co.uk>
2020-02-04 12:07:21 +0000

    Translate from notebook to script

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,113 +1,66 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 from pathlib import Path
-
-notebook_dir = Path('data')
-data_file_paths = [p for p in notebook_dir.iterdir()]
-
-
-# In[2]:
-
-
-from collections import OrderedDict
+from collections import namedtuple
 import numpy as np
 import re
-
-data_dict = dict()
-
-for path in data_file_paths:
-    matched = re.search('data/(.+?).csv', str(path))
-    frac = matched.group(1)
-    data = np.loadtxt(path, dtype=int, delimiter=',')
-    data_avg = np.mean(data, axis=0)/(100*100)
-    data_std = np.std(data, axis=0)/(100*100)
-    data_dict[frac] = (data_avg, data_std)
-    
-sorted_data_dict = OrderedDict(sorted(data_dict.items(), key=lambda t: float(t[0])))
-sorted_data_dict
-
-
-# In[3]:
-
-
-print("Read in fractions:")
-_ = [print(frac) for frac in sorted_data_dict]
-
-
-# In[4]:
-
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
 from itertools import cycle
 
-fig, ax = plt.subplots(figsize=(15, 15))
+BOARD_WIDTH = 100
+BOARD_HEIGHT = 100
+BOARD_SIZE = BOARD_WIDTH * BOARD_HEIGHT
 
-colors = cycle(['C'+str(n) for n in range(9)])
-
-for frac, color in zip([f for f in sorted_data_dict if len(f)<4], colors):
-    y, yerr = sorted_data_dict.get(frac)
-    x = np.arange(len(y))
-    y_upper = y + yerr
-    y_lower = y - yerr
-    ax.plot(x, y, label=frac, color=color)
-    ax.fill_between(x, y_upper, y_lower, facecolor=color, alpha=0.2)
-
-ax.set_xlabel("Iterations")
-ax.set_ylabel("Fraction Alive")
-ax.legend()
-
-fig.savefig("full_results.png", dpi=150)
+DATA_DIR = 'data'
 
 
-# In[5]:
+def read_files(dir_path):
+    """Read data from the runner's output directory and return a list of
+    data Series objects, one for each file in the directory. Each Series
+    object has the corresponding starting fraction, and average and
+    standard deviation values at each iteration."""
+
+    data_list = []
+    Series = namedtuple('Series', 'frac avg std')
+
+    data_file_paths = (p for p in dir_path.iterdir())
+
+    for path in data_file_paths:
+        # Pull fraction from filename
+        matched = re.search('data/(.+?).csv', str(path))
+        frac = matched.group(1)
+        # Add to data_dict
+        data = np.loadtxt(path, dtype=int, delimiter=',')
+        data_avg = np.mean(data, axis=0) / BOARD_SIZE
+        data_std = np.std(data, axis=0) / BOARD_SIZE
+        data_list.append(Series(float(frac), data_avg, data_std))
+
+    return data_list
 
 
-ax.set_ylim(0, 0.2)
-
-fig
-
-
-# In[6]:
-
-
-ax.set_xlim(0, 250)
-
-fig
-
-
-# In[8]:
-
-
-fig, ax = plt.subplots(figsize=(15, 15))
-
-for frac, color in zip([f for f in sorted_data_dict if 0 < float(f) < 0.3], colors):
-    y, yerr = sorted_data_dict.get(frac)
-    x = np.arange(len(y))
-    y_upper = y + yerr
-    y_lower = y - yerr
-    ax.plot(x, y, label=frac, color=color)
-    ax.fill_between(x, y_upper, y_lower, facecolor=color, alpha=0.2)
-
-ax.set_xlabel("Iterations")
-ax.set_ylabel("Fraction Alive")
-ax.legend()
-
-fig.savefig("full_results.png", dpi=150)
+def plot_fig(data):
+    fig, ax = plt.subplots()
+    colors = cycle(['C'+str(n) for n in range(9)])
+    for s, c in zip(data, colors):
+        frac, y, y_std = s
+        x = np.arange(y.size)
+        y_upper = y + y_std
+        y_lower = y - y_std
+        ax.plot(x, y, label=frac, color=c)
+        ax.fill_between(x, y_upper, y_lower, facecolor=c, alpha=0.2)
+    ax.set_xlabel("Iterations")
+    ax.set_ylabel("Fraction Alive")
+    ax.legend()
+    fig.show()
 
 
-# In[ ]:
+def main():
+    dir_path = Path(DATA_DIR)
+    data = read_files(dir_path)
+    data_full_range = (s for s in data if len(str(s.frac)) == 3)
+    plot_fig(data_full_range)
 
 
-
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
